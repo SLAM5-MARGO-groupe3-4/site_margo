c3febcf4bb4afccafbbcd045ae2d29079e7e105f
btssio <btssio@btssio-VirtualBox>
2014-12-08 17:22:50 +0100

    sgrg

--- site_margo/include/head.php
+++ site_margo/include/head.php
@@ -8,8 +8,6 @@ $niveauAcces = $_SESSION['niveauAcces'];
 require_once('connexion.php');
 //require_once('fonction.php');
 ?>
-<script type="text/javascript" src="../js/verif.js"></script>
-<link type="text/css" rel="stylesheet" href="../css/style.css">
-
-<!-- Latest compiled and minified CSS -->
-<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">-->
+<meta charset="utf-8" >
+<script type="text/javascript" src="js/verif.js"></script>
+<link type="text/css" rel="stylesheet" href="css/style.css">
