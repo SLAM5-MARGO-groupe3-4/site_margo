1af8f1d2db3fff6d8d7cfa95388ff12caf0345bd
btssio <btssio@btssio-VirtualBox>
2014-12-09 11:32:28 +0100

    edt

--- site_margo/emploiDuTemps.php
+++ site_margo/emploiDuTemps.php
@@ -3,8 +3,34 @@ include('include/head.php');
 
 $tabJours = array("Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi");
 $tabHeures = array("8h-8h55", "8h55-9h", "9h-9h50", "10h05-11h", "11h-11h55", "11h55-12h50", "13h10-14h05", '14h05-15h', "15h15-16h10", "16h10-17h05", "17h05-18h");
-
+$tabTout = array();
 $espace='Emploi du temps';
+
+$sqlEdt = "SELECT CODE_CLASSE FROM APPARTENIR WHERE IDPERS=".$id;
+$rEdt = mysql_query($sqlEdt, $link);
+$tLog = mysql_fetch_array($rEdt);
+
+$sqlRelationEdt = "SELECT IDEDT FROM RELATION_EC WHERE CODE_CLASSE=".$tLog[0];
+$rRelationEdt = mysql_query($sqlRelationEdt, $link);
+$tRelationEdt = mysql_fetch_array($rRelationEdt);
+
+$sqlRelationMj = "SELECT CODEMAT FROM RELATION_MJ WHERE IDEDT=".$tRelationEdt[0];
+$rRelationMj = mysql_query($sqlRelationMj, $link);
+$tRelationMj = mysql_fetch_array($rRelationMj);
+
+$sqlClasse = "SELECT LIBELLE FROM CLASSE WHERE CODE_CLASSE=".$tRelationMj[0];
+$rClasse = mysql_query($sqlClasse, $link);
+$tClasse = mysql_fetch_array($rClasse);
+
+$sqlJour="SELECT IDJOUR FROM REL_3 WHERE IDEDT=".$tRelationEdt;
+$rJour = mysql_query($sqlJour, $link);
+$tJour = mysql_fetch_array($rJour);
+
+$sqlLib = "SELECT LIBELLE FROM JOUR WHERE IDJOUR=".$tJour[0];
+$rLib = mysql_query($sqlLib, $link);
+$tLib = mysql_fetch_array($rLib);
+
+
 
 /* echo '<table border="1">';
   echo '<th></th>';
@@ -54,7 +80,7 @@ $espace='Emploi du temps';
                 echo '<tr>';
                 echo '<td>' . $tabHeures[$j] . '</td>';
                 for ($i = 0; $i < count($tabJours); $i++) {
-                    echo '<td>' . 't' . '</td>';
+                    echo '<td>' . $tClasse . '</td>';
                 }
                 echo'</tr>';
             }
