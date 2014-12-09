fa2020c01a52fabe0142fc118d0cd000b6c3130c
btssio <btssio@btssio-VirtualBox>
2014-12-09 11:16:08 +0100

    Personnes CSS Accueil

--- site_margo/accueil.php
+++ site_margo/accueil.php
@@ -1,6 +1,10 @@
 <?php
 //session_start();
 require_once('include/head.php');
+$niv = $_SESSION['niveauAcces'];
+
+$texteAdmin = "<h2>Depuis ce site, vous pouvez :<ul><li>Accèder à votre emploi du temps</li><li>Rechercher une personne</li><li>Voir vos notes</li><li>Voir et rechercher vos informations personnelles</li></ul></h2>";
+$texteEtu = "<h2>Depuis ce site, vous pouvez :<ul><li>Accèder à votre emploi du temps</li><li>Voir vos notes</li><li>Voir et rechercher vos informations personnelles</li></ul></h2>";
 ?>
 <!DOCTYPE html>
 <html>
@@ -28,7 +32,24 @@ require_once('include/head.php');
         </aside>
         <section>
             <h1>Accueil</h1>
-            <h2>Depuis ce site, vous pouvez :<ul><li>Accèder à votre emploi du temps</li><li>Voir votre bulletin</li><li>Voir et modifier vos informations personnelles</li></ul></h2>
+            <?php
+            switch ($niv) {
+                case 1 : // Admin
+                    echo $texteAdmin;
+                    break;
+                case 2 : // Professeurs
+                    echo $texteAdmin;
+                    break;
+                case 3 : //Techniciens
+                    echo $texteAdminA;
+                    break;
+                case 4 : //Etudiants
+                    echo $texteEtu;
+                    break;
+                default :
+                    echo "Erreur";
+            }
+            ?>
         </section>
         <footer>
             <p>LAURENT - LE ROUX - NOULET - URBAIN | Copyright 2014-2015</p>
